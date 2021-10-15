3e9f6b3451a8d1e489d5c61044f2e1f5fcd412fd
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-15 19:12:36 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from "./components/Header";
+import Body from './components/Body'
 
 function App() {
   return (
     <div className="App">
-        My Project
+      <Header />
+      <Body/>
     </div>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+const Body = () => {
+  return (
+    <div>
+      <h1>My Body</h1>
+    </div>
+  );
+};
+
+export default Body;
