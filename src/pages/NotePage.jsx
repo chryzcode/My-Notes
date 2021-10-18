912a448d45ce51ab86c2993745a5302ce2a37cab
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-18 11:09:28 +0100

    commit

---
+++ src/pages/NotePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotePage = () => {
+    return (
+        <div>
+            <h1>This is a single note page</h1>
+        </div>
+    )
+}
+
+export default NotePage
