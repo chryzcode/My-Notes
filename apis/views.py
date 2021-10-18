b6ca2cc0537a151c10e45f5cf024e1d8f031a132
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-18 16:12:53 +0100

    commit

--- apis/views.py
+++ apis/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -35,4 +39,17 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)  
