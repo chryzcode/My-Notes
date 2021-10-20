bff613c65496daaad6c3548b8107e03f95ee9eb5
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-10-20 07:31:00 +0100

    push

--- notes_app/views.py
+++ notes_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 # Create your views here.
-
+@login_required(login_url='login')
 def myNotesList(request):
     context = {}
     notes = Note.objects.filter(user=request.user)
